b51aa53efcc3e4300bde4df822b179fd52d05776
Andrey Klemenko <web.klemenko@gmail.com>
2021-01-24 23:52:31 +0200

    complete task 3

---
+++ js/task-3.js
@@ -0,0 +1,47 @@
+//Напиши скрипт для создания галлереи изображений по массиву данных.
+// Используй массив объектов images для создания тегов img вложенных в li.
+// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const galleryRef = document.querySelector('#gallery');
+galleryRef.setAttribute(
+  'style',
+  'list-style:none; display: flex; justify-content: space-around',
+);
+
+const galleryCreate = item =>
+  `<li><img src='${item.url}' alt='${item.alt}' width='200' height='150'></li>`;
+
+const galleryList = images.reduce((acc, item) => acc + galleryCreate(item), '');
+
+const insertItems = galleryRef.insertAdjacentHTML('beforeend', galleryList);
+
+//    =====Способ 2=======
+// const galleryCreate = (ref, obj) => {
+//   obj.map(item => {
+//     ref.insertAdjacentHTML(
+//       'beforeend',
+//       ` <li> <img src=${item.url} alt=${item.alt} width='200' height='150'> </li>`,
+//     );
+//   });
+// };
+// galleryCreate(galleryRef, images);
